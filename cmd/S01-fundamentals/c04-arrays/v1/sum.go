660027e5d8997a9cd60d0ebc548104091be52ae6
Tom Wilson <tom.andrew.wilson@gmail.com>
2022-06-12 09:22:09 -0400

    better cmd for c19 and c4

--- cmd/S01-fundamentals/c04-arrays/v1/sum.go
+++ cmd/S01-fundamentals/c04-arrays/v1/sum.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	arrays "learn.go/S01-fundamentals/c04-arrays/v1"
 )
 
 func main() {
-	
-	println(arrays.Sum([5]int {1, 2, 3, 4, 5})
+	inputs_array := [5]int{1, 2, 3, 4, 5}
+	output_result := arrays.Sum(inputs_array)
+	fmt.Println("output_result =", output_result)
+
 }
--- cmd/S01-fundamentals/c19-generics/stack.go
+++ cmd/S01-fundamentals/c19-generics/stack.go
@@ -1,25 +1,42 @@
-package generics
+package main
 
-type Stack[T any] struct {
-	values []T
-}
+import (
+	"fmt"
 
-func (s *Stack[T]) Push(value T) {
-	s.values = append(s.values, value)
-}
+	generics "learn.go/S01-fundamentals/c19-generics/v1"
+)
 
-func (s *Stack[T]) IsEmpty() bool {
-	return len(s.values) == 0
-}
+func main() {
+	fmt.Println("make a new stack")
+	myStackOfInts := new(generics.Stack[int])
+	fmt.Println("check stack is empty")
+	fmt.Println("myStackOfInts.IsEmpty =", myStackOfInts.IsEmpty())
 
-func (s *Stack[T]) Pop() (T, bool) {
-	if s.IsEmpty() {
-		var zero T
-		return zero, false
-	}
+	fmt.Println("add a thing, then check it's not empty")
+	fmt.Println("push 123 to stack")
+	myStackOfInts.Push(123)
+	fmt.Println("myStackOfInts.IsEmpty =", myStackOfInts.IsEmpty())
 
-	index := len(s.values) - 1
-	el := s.values[index]
-	s.values = s.values[:index]
-	return el, true
+	fmt.Println("add another thing, pop it back again")
+	fmt.Println("push 456 to stack")
+	myStackOfInts.Push(456)
+	fmt.Println("Pop!")
+	value, _ := myStackOfInts.Pop()
+	fmt.Println("value =", value, "expected = ", 456)
+	fmt.Println("Pop!")
+	value, _ = myStackOfInts.Pop()
+	fmt.Println("value =", value, "expected = ", 123)
+	fmt.Println("myStackOfInts.IsEmpty =", myStackOfInts.IsEmpty())
+
+	fmt.Println("can get the numbers we put in as numbers, not untyped interface{}")
+	fmt.Println("Push a 1")
+	myStackOfInts.Push(1)
+	fmt.Println("Push a 2")
+	myStackOfInts.Push(2)
+	fmt.Println("Pop!")
+	firstNum, _ := myStackOfInts.Pop()
+	fmt.Println("Pop!")
+	secondNum, _ := myStackOfInts.Pop()
+	fmt.Println("firstNum+secondNum =", firstNum+secondNum, "expected = ", 3)
+
 }
